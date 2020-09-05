1dabfb0bcabf785448f77cfd3f1a4c496e6282d7
K Dheeraj  Kumar <56029049+DheerajKumar97@users.noreply.github.com>
2020-09-05 21:56:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,url_for
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
